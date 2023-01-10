d1e16dfd8f26b59cfefb27bce5ef6eb4abe989b3
Tanner Horton <Mtannerar13@gmail.com>
2023-01-09 21:43:31 -0500

    updating after workign through errors with Danila

--- app.py
+++ app.py
@@ -1,35 +1,95 @@
 
-import numpy as np
+# import numpy as np
 
+# import sqlalchemy
+# from sqlalchemy.ext.automap import automap_base
+# from sqlalchemy.orm import Session
+# from sqlalchemy import create_engine, func
+
+# from flask import Flask, jsonify
+
+# #################################################
+# # Database Setup
+# #################################################
+# engine = create_engine("postgresql://postgres:0206Teddy@localhost:5432/CO2_Emissions")
+
+# # reflect an existing database into a new model
+# Base = automap_base()
+# # reflect the tables
+# Base.prepare(engine, reflect=True)
+
+# # Save reference to the table
+# Emissions = Base.classes.emissions
+
+# #################################################
+# # Flask Setup
+# #################################################
+# app = Flask(__name__)
+
+# #################################################
+# # Flask Routes
+# #################################################
+
+# @app.route("/")
+# def welcome():
+#     """List all available api routes."""
+#     return (
+#         f"Available Routes:<br/>"
+#         f"/api/v1.0/countries<br/>"
+#     )
+
+# @app.route("/api/v1.0/countries")
+# def names():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
+
+#     """Return a list of all countries"""
+#     # Query all passengers
+#     results = session.query(Emissions.Country).distinct().all()
+
+#     session.close()
+
+#     # Convert list of tuples into normal list
+#     all_countries = list(np.ravel(results))
+
+#     return jsonify(all_countries)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+
+import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, render_template
+import psycopg2
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("postgresql://postgres:rossygossy64@localhost:5432/C02_Emissions")
-
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(engine, reflect=True)
-
-# Save reference to the table
-Emissions = Base.classes.emissions
-
+DB_NAME = "CO2_emissions"
+DB_USER = "postgres"
+DB_PASS = "0206Teddy"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+ 
+    
+engine = create_engine("postgresql://postgres:0206Teddy@localhost:5432/CO2_emissions")
+conn = psycopg2.connect(database=DB_NAME,
+                            user=DB_USER,
+                            password=DB_PASS,
+                            host=DB_HOST,
+                            port=DB_PORT)
+cur = conn.cursor()
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
-
 #################################################
 # Flask Routes
 #################################################
-
 @app.route("/")
 def welcome():
     """List all available api routes."""
@@ -37,27 +97,24 @@ def welcome():
         f"Available Routes:<br/>"
         f"/api/v1.0/countries<br/>"
     )
-
 @app.route("/api/v1.0/countries")
 def names():
     # Create our session (link) from Python to the DB
-    session = Session(engine)
-
+    # session = Session(engine)
     """Return a list of all countries"""
     # Query all passengers
-    results = session.query(Emissions.Country).distinct().all()
-
-    session.close()
-
-    # Convert list of tuples into normal list
-    all_countries = list(np.ravel(results))
-
-    return jsonify(all_countries)
-
+    results = engine.execute("SELECT DISTINCT COUNTRY FROM emissions").fetchall()
+    
+    countries = list(np.ravel(results))
+    return jsonify(countries)
+# # for i in results:
+#         print (i)
+# # print(results)
+# #     # results = session.query(Emissions.Country).distinct().all()
+# #     #session.close()
+# #     # Convert list of tuples into normal list
+# #     all_countries = list(np.ravel(results))
+# #     return jsonify(all_countries)
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
  
